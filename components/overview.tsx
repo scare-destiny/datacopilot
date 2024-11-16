051880ac003626adb4b5705ec6fdc63deb8d7c35
scare-destiny <zhenya.venger@gmail.com>
2024-11-16 11:03:03 +0100

    x

--- components/overview.tsx
+++ components/overview.tsx
@@ -1,52 +1,34 @@
-import { motion } from 'framer-motion';
-import Link from 'next/link';
+import { motion } from 'framer-motion'
+import Link from 'next/link'
 
-import { MessageIcon, VercelIcon } from './icons';
+import { MessageIcon, DataCopilotIcon } from './icons'
 
 export const Overview = () => {
-  return (
-    <motion.div
-      key="overview"
-      className="max-w-3xl mx-auto md:mt-20"
-      initial={{ opacity: 0, scale: 0.98 }}
-      animate={{ opacity: 1, scale: 1 }}
-      exit={{ opacity: 0, scale: 0.98 }}
-      transition={{ delay: 0.5 }}
-    >
-      <div className="rounded-xl p-6 flex flex-col gap-8 leading-relaxed text-center max-w-xl">
-        <p className="flex flex-row justify-center gap-4 items-center">
-          <VercelIcon size={32} />
-          <span>+</span>
-          <MessageIcon size={32} />
-        </p>
-        <p>
-          This is an{' '}
-          <Link
-            className="font-medium underline underline-offset-4"
-            href="https://github.com/vercel/ai-chatbot"
-            target="_blank"
-          >
-            open source
-          </Link>{' '}
-          chatbot template built with Next.js and the AI SDK by Vercel. It uses
-          the{' '}
-          <code className="rounded-md bg-muted px-1 py-0.5">streamText</code>{' '}
-          function in the server and the{' '}
-          <code className="rounded-md bg-muted px-1 py-0.5">useChat</code> hook
-          on the client to create a seamless chat experience.
-        </p>
-        <p>
-          You can learn more about the AI SDK by visiting the{' '}
-          <Link
-            className="font-medium underline underline-offset-4"
-            href="https://sdk.vercel.ai/docs"
-            target="_blank"
-          >
-            docs
-          </Link>
-          .
-        </p>
-      </div>
-    </motion.div>
-  );
-};
+	return (
+		<motion.div
+			key='overview'
+			className='max-w-3xl mx-auto md:mt-20'
+			initial={{ opacity: 0, scale: 0.98 }}
+			animate={{ opacity: 1, scale: 1 }}
+			exit={{ opacity: 0, scale: 0.98 }}
+			transition={{ delay: 0.5 }}
+		>
+			<div className='rounded-xl p-6 flex flex-col gap-8 leading-relaxed text-center max-w-xl'>
+				<p className='flex flex-row justify-center gap-4 items-center'>
+					<DataCopilotIcon size={46} />
+					{/* <span>+</span>
+					<MessageIcon size={32} /> */}
+				</p>
+				<p>
+					Turn Complex Data Into Clear Decisions. Stop wrestling with SQL queries and
+					complex analytics tools. DataCopilot is your smart data companion that
+					speaks human, not tech.
+				</p>
+				<p>
+					Get queries for any database, ask questions in plain English, and get
+					instant insights that drive your business forward.
+				</p>
+			</div>
+		</motion.div>
+	)
+}
